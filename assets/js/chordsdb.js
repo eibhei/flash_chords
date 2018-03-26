7bbbb01219e1fdee9e6cbcd69eb145141e8df29a
Bill O'Dwyer <BODwyer@stewartslaw.com>
2018-03-26 18:00:39 +0100

    Rough implementation of new chordsDB

---
+++ assets/js/chordsdb.js
@@ -0,0 +1,33 @@
+// A
+    suffix: Major
+    positions: [
+      {
+        frets:"x02220",
+        fingers:"001230"
+      },
+      {
+        frets:"x02225",
+        fingers:"001114",
+        barres:2
+      },
+      {
+        frets:'xceeex',
+        fingers:'013330',
+        barres:14
+
+      }
+    ]
+
+etc
+
+// Bb
+// B
+// C
+// C#
+// D
+// Eb
+// E
+// F
+// F#
+// G
+// Ab
